526269aea64de5148439020a37935b0f256bba0f
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2021-04-12 15:05:56 -0300

    auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,8 @@
 import { TokenService } from './token.service';
-import { HttpClient, HttpErrorResponse } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
+import { tap, catchError } from 'rxjs/operators';
 
 const OAUTH_CLIENT = 'express-client';
 const OAUTH_SECRET = 'express-secret';
@@ -40,4 +41,58 @@ export class AuthService {
     private http: HttpClient,
     private tokenService: TokenService
   ) { }
+
+  login(loginData: any): Observable<any>{
+    this.tokenService.removeAccessToken();
+    this.tokenService.removeRefreshToken();
+
+    const body = new HttpParams()
+      .set('username', loginData.username)
+      .set('password', loginData.password)
+      .set('grant_type', 'password');
+
+      return this.http.post<any>(API_URL + 'oauth/token', body, HTTP_OPTIONS)
+      .pipe(
+        tap(res => {
+          this.tokenService.saveToken(res.access_token);
+          this.tokenService.saveRefreshToken(res.refresh_token);
+        }),
+        catchError(AuthService.handleError)
+      );
+  }
+
+  refreshToken(refreshData: any): Observable<any> {
+    this.tokenService.removeToken();
+    this.tokenService.removeRefreshToken();
+    const body = new HttpParams()
+      .set('refresh_token', refreshData.refresh_token)
+      .set('grant_type', 'refresh_token');
+    return this.http.post<any>(API_URL + 'oauth/token', body, HTTP_OPTIONS)
+      .pipe(
+        tap(res => {
+          this.tokenService.saveToken(res.access_token);
+          this.tokenService.saveRefreshToken(res.refresh_token);
+        }),
+        catchError(AuthService.handleError)
+      );
+  }
+
+  logout(): void {
+    this.tokenService.removeToken();
+    this.tokenService.removeRefreshToken();
+  }
+
+  register(data: any): Observable<any> {
+    return this.http.post<any>(API_URL + 'oauth/signup', data)
+      .pipe(
+        tap(_ => AuthService.log('register')),
+        catchError(AuthService.handleError)
+      );
+  }
+
+  secured(): Observable<any> {
+    return this.http.get<any>(API_URL + 'secret')
+      .pipe(catchError(AuthService.handleError));
+  }
 }
+
