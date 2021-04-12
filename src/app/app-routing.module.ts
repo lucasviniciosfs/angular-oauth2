eb581682d6a399c9d59217dd485d9940b9c8d344
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2021-04-12 15:04:38 -0300

    routing and module root

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SecureComponent } from './components/secure/secure.component';
+import { AuthGuard } from './guards/auth.guard';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'secure', pathMatch: 'full' },
+  { path: 'secure', canActivate: [ AuthGuard ], component: SecureComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: '404', component: NotFoundComponent },
+  { path: '**', redirectTo: '404' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
