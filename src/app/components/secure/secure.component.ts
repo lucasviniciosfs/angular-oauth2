0916af42a692fb504a449ef6a172c9acff3024f7
lucasviniciosfs <lucasvinicios-fs@hotmail.com>
2021-04-13 09:44:48 -0300

    secure component

--- src/app/components/secure/secure.component.ts
+++ src/app/components/secure/secure.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-secure',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SecureComponent implements OnInit {
 
-  constructor() { }
+  message = '';
+  isLoadingResults = false;
+
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    this.isLoadingResults = true;
+    this.authService.secured()
+      .subscribe((data: any) => {
+        this.message = data;
+        console.log(data);
+        this.isLoadingResults = false;
+      });
+  }
+
+  logout(): void {
+    this.authService.logout();
+    this.router.navigate(['/login']).then(_ => console.log('Logout'));
   }
 
 }
